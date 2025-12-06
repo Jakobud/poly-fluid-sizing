b36a8d5da7abee62cd72456e7976d98133048651
Jake Wilson <jake.e.wilson@gmail.com>
2025-12-05 23:00:10 -0700

    Added tests for linear-interpolation, list-sort and map-sort

--- test/all.test.js
+++ test/all.test.js
@@ -3,3 +3,6 @@ const sassTrue = require('sass-true');
 
 sassTrue.runSass({ describe, it }, path.join(__dirname, 'poly-fluid-sizing.spec.scss'));
 sassTrue.runSass({ describe, it }, path.join(__dirname, 'list-remove.spec.scss'));
+sassTrue.runSass({ describe, it }, path.join(__dirname, 'list-sort.spec.scss'));
+sassTrue.runSass({ describe, it }, path.join(__dirname, 'map-sort.spec.scss'));
+sassTrue.runSass({ describe, it }, path.join(__dirname, 'linear-interpolation.spec.scss'));
